cbe5f0804e98b8653eacc6d532c4b8e1bc9b54d6
HappyJoe777 <vescko332@gmail.com>
2022-10-05 11:22:50 +0300

    Added Views, Controllers and API calls

---
+++ services/APICalls.js
@@ -0,0 +1,4 @@
+export const rawgAPIKey = () => process.env.RAWG_GAMING_API_KEY;
+
+export const XRapidAPIKey = process.env.RAPID_API_KEY;
+export const XRapidAPIHost = 'rawg-video-games-database.p.rapidapi.com';
